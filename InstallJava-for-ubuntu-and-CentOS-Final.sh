47a03819198d30c699e504d4141892aa8f832376
Amit Saini <asaini@miraclesoft.com>
2017-02-20 16:06:46 -0500

    first commit

---
+++ InstallJava-for-ubuntu-and-CentOS-Final.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -f /etc/debian_version ]; then
+    echo "Ubuntu"
+	apt-get update
+	
+elif [ -f /etc/redhat-release ]; then
+    echo "Red Hat and CentOS"
+	#yum -y update
+fi
+wget --no-cookies \
+--no-check-certificate \
+--header "Cookie: oraclelicense=accept-securebackup-cookie" \
+"http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz" \
+-O jdk-8u111-linux-x64.tar.gz
+# then
+tar -xzvf jdk-8u111-linux-x64.tar.gz -C /usr/local/
+
+
+fi
+
---
+++ script.py
@@ -0,0 +1,3 @@
+from subprocess import call
+import shlex
+call(shlex.split('bash InstallJava-for-ubuntu-and-CentOS-Final.sh'))
